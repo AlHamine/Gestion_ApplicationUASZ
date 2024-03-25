2f716ee691fe1ab53affee48ea079f1470467cf6
AlHamine <oriontheroot@gmail.com>
2024-03-25 16:19:12 +0000

    Initialisation des partie back-end

---
+++ src/main/java/uasz/groupe6/Gestion_ApplicationUASZ/GestionApplicationUaszApplication.java
@@ -0,0 +1,13 @@
+package uasz.groupe6.Gestion_ApplicationUASZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionApplicationUaszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionApplicationUaszApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uasz/groupe6/Gestion_ApplicationUASZ/GestionApplicationUaszApplicationTests.java
@@ -0,0 +1,13 @@
+package uasz.groupe6.Gestion_ApplicationUASZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionApplicationUaszApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
